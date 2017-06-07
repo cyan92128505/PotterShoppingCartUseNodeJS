bbb4faeea2c1d0ef05b5c2ddc31467a4ae220bf7
cyan92128505 <cyan92128505@gmail.com>
2017-06-07 20:06:03 +0800

    Test '第一集買了一本，其他都沒買，價格應為100*1=100元'

--- PotterShoppingCart.js
+++ PotterShoppingCart.js
@@ -1,5 +1,41 @@
+var PotterBookInfoList = [{
+        name: '哈利波特第一集',
+        number: 0
+    },
+    {
+        name: '哈利波特第二集',
+        number: 0
+    },
+    {
+        name: '哈利波特第三集',
+        number: 0
+    },
+    {
+        name: '哈利波特第四集',
+        number: 0
+    },
+    {
+        name: '哈利波特第五集',
+        number: 0
+    }
+];
 
-function PotterShoppingCart (){
+
+function PotterShoppingCart(Cart, callback) {
+    var sum = 0;
+    Cart.forEach((cartValue, index) => {
+        PotterBookInfoList.forEach((book) => {
+            if (book.name === cartValue) {
+                book.number++;
+            }
+        });
+    });
+
+    PotterBookInfoList.forEach((book)=>{
+        sum = sum + book.number*100;
+    });
+
+    return sum;
 }
 
 module.exports = PotterShoppingCart;
--- test/PotterShoppingCart.test.js
+++ test/PotterShoppingCart.test.js
@@ -4,14 +4,15 @@ const expect = require('chai').expect;
 describe('PotterShoppingCart', () => {
     it('第一集買了一本，其他都沒買，價格應為100*1=100元', () => {
         // arrange
-        var target = require(path.join(process.cwd(), 'PotterShoppingCart.js'));
-        target(['哈利波特第一集']);
-        var expected = 100;
+        var PotterShoppingCart = require(path.join(process.cwd(), 'PotterShoppingCart.js'));
+        PotterShoppingCart(['哈利波特第一集'], (target) => {
+            var expected = 100;
 
-        // act
-        var actual = target();
+            // act
+            var actual = target;
 
-        // assert
-        expect(actual).to.equal(expected);
+            // assert
+            expect(actual).to.equal(expected);
+        });
     });
 })
